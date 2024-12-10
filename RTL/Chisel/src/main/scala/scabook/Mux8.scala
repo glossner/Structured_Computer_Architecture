d4f1156f0e35b369286bec5c5a6d337fbe5acd58
glossner <john@glossner.org>
2024-12-10 14:05:18 -0500

    Mux8 scala

---
+++ RTL/Chisel/src/main/scala/scabook/Mux8.scala
@@ -0,0 +1,17 @@
+// Licensed under the BSD 3-Clause License. 
+// See https://opensource.org/licenses/BSD-3-Clause for details.
+
+package scabook
+
+import chisel3._
+
+
+class Mux8 extends Module {
+  val io = IO(new Bundle {
+    val inputs = Input(Vec(4, UInt(8.W)))
+    val select = Input(UInt(2.W))
+    val output = Output(UInt(8.W))
+  })
+
+  io.output := Multiplexer(io.inputs, io.select)
+}
