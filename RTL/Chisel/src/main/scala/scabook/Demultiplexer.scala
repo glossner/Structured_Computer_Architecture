5bd468302b96a7826e09d9ab8e52544eebb1df5f
glossner <john@glossner.org>
2024-12-24 10:47:19 -0500

    Demultiplexer with UInt I/O and passing unit tests

--- RTL/Chisel/src/main/scala/scabook/Demultiplexer.scala
+++ RTL/Chisel/src/main/scala/scabook/Demultiplexer.scala
@@ -6,17 +6,17 @@ package scabook
 import chisel3._
 import chisel3.util._
 
-class Demultiplexer[T <: Data](dataType: T, numOutputs: Int) extends Module {
+class Demultiplexer(numOutputs: Int, width: Int) extends Module {
   require(isPow2(numOutputs), "Number of outputs must be a power of 2")
 
   val io = IO(new Bundle {
-    val input = Input(dataType)
-    val select = Input(UInt(log2Ceil(numOutputs).W))
-    val outputs = Output(Vec(numOutputs, dataType))
+    val input = Input(UInt(width.W))                     // Single `UInt` input
+    val select = Input(UInt(log2Ceil(numOutputs).W))     // Select signal
+    val outputs = Output(Vec(numOutputs, UInt(width.W))) // Vec of `UInt` outputs
   })
 
   // Default all outputs to zero
-  io.outputs.foreach(_ := 0.U.asTypeOf(dataType))
+  io.outputs.foreach(_ := 0.U)
 
   // Drive the selected output
   io.outputs(io.select) := io.input
@@ -24,8 +24,10 @@ class Demultiplexer[T <: Data](dataType: T, numOutputs: Int) extends Module {
 
 // Companion object for easier instantiation
 object Demultiplexer {
-  def apply[T <: Data](input: T, select: UInt, numOutputs: Int): Vec[T] = {
-    val demux = Module(new Demultiplexer(input.cloneType, numOutputs))
+  def apply(input: UInt, select: UInt, numOutputs: Int): Vec[UInt] = {
+    require(numOutputs > 0, "Number of outputs must be greater than zero.")
+    require(isPow2(numOutputs), "Number of outputs must be a power of 2.")
+    val demux = Module(new Demultiplexer(numOutputs, input.getWidth))
     demux.io.input := input
     demux.io.select := select
     demux.io.outputs
