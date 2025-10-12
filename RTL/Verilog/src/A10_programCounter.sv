57ad52e4aeeadbdb8b7ce7f0e0295ab2745d7e31
John Glossner <john@glossner.org>
2025-10-12 12:30:14 +0000

    Move slides to Latex folder so latexcad can be picked up. Revise Chapter 8 slides.

--- RTL/Verilog/src/A10_programCounter.sv
+++ RTL/Verilog/src/A10_programCounter.sv
@@ -1,23 +1,24 @@
 module programCounter #(parameter n=32)
-        (   output  logic [31:0]    progAddr,
+        (   output  logic [n-1:0]   progAddr,
             input   logic [2:0]     sel     ,
-            input   logic [31:0]    relValue,
-            input   logic [31:0]    absValue,
+            input   logic [n-1:0]   relValue,
+            input   logic [n-1:0]   absValue,
             input   logic           reset   ,
             input   logic           clock   );
-    logic [31:0]    pc      ;
-    logic [31:0]    nextPC  ;
+    logic [n-1:0]  	pc      ;
+    logic [n-1:0]  	nextPC  ;
 
     always_ff @(posedge clock) if (reset)   pc <= 0     ;
                                 else        pc <= nextPC;
     always_comb
      case(sel)
-         3'b000: nextPC = pc                                        ;
-         3'b001: nextPC = pc + relValue                             ;
-         3'b010: nextPC = (absValue == 0) ? pc + relValue : pc + 1  ;
-         3'b011: nextPC = (absValue == 0) ? pc + 1 : pc + relValue  ;
-         3'b100: nextPC = absValue                                  ;
-        default: nextPC = pc + 1                                    ;
+         3'b000: nextPC = pc + 1                                    ;
+         3'b001: nextPC = pc                                        ;
+         3'b010: nextPC = pc + relValue                             ;
+         3'b011: nextPC = (absValue == 0) ? pc + relValue : pc + 1  ;
+         3'b100: nextPC = (absValue == 0) ? pc + 1 : pc + relValue  ;
+		3'b101: nextPC = absValue									;
+        default: nextPC = pc 	                                    ;
      endcase
 
      assign progAddr = pc       ; // for pipelined version
